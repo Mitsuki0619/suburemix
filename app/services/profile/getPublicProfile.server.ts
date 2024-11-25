8b51d4a35b0cf329ae2ed80592573dc9eb898a3a
Mitsuki0619 <boc.fsfb@gmail.com>
2024-11-25 23:32:07 +0900

    refactor: update header component and add profile page
    - Update the header component in the app/components/header.tsx file to improve the formatting and add a link to the home page.
    - Add a new profile page in the app/routes/_public+/$userId+/profile+/_index.tsx file to display user profile information, including name, avatar, bio, and blogs.
    - Rename the getProfile function to getPrivateProfile and create a new getPublicProfile function in the app/services/profile/ directory to retrieve public user profile information.
    - Update the loader function in the app/routes/_public+/$userId+/profile+/edit+/_index.tsx file to use the getPrivateProfile function instead of the getProfile function.
    - Rename the getProfile function to getPrivateProfile in the app/services/profile/getProfile.server.ts file.
    - Add new columns to the Blog table in the prisma/migrations/20241125141142_/migration.sql file to track the creation and publication status of blog posts.
    - Update the schema.prisma file to reflect the changes made to the Blog table structure.

---
+++ app/services/profile/getPublicProfile.server.ts
@@ -0,0 +1,33 @@
+import { User } from '@prisma/client/edge'
+import { AppLoadContext } from '@remix-run/cloudflare'
+
+export const getPublicProfile = async (
+  context: AppLoadContext,
+  userId: User['id'],
+  myUserId?: User['id']
+) => {
+  const user = await context.db.user.findUnique({
+    where: { id: userId },
+    select: {
+      id: true,
+      name: true,
+      role: true,
+      image: true,
+      blogs: {
+        where: {
+          published: myUserId !== userId,
+        },
+      },
+    },
+  })
+  const profile = await context.db.profile.findUnique({
+    where: { userId },
+    select: {
+      bio: true,
+    },
+  })
+  if (!user) {
+    throw new Error('User not found')
+  }
+  return { ...user, ...profile }
+}
